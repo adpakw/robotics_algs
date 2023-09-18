6df287d3a17b27eae8d07d13c9ca7964d8738eb8
adpak <adpakw@gmail.com>
2023-09-19 01:04:01 +0300

    distbug steps and Make

--- bug_alg/scripts/distbug.py
+++ bug_alg/scripts/distbug.py
@@ -17,7 +17,6 @@ import math
 srv_client_go_to_point_ = None
 srv_client_wall_follower_ = None
 yaw_ = 0
-yaw_error_allowed_ = 5 * (math.pi / 180) # 5 degrees
 #get the innitial position coordinates
 position_ = Point()
 initial_position_ = Point()
@@ -31,14 +30,15 @@ desired_position_.y = rospy.get_param('des_pos_y')
 desired_position_.z = 0
 regions_ = None
 #states of robot during algorithm
-state_desc_ = ['Go to point', 'wall following', 'checking leave point', 'start']
+state_desc_ = ['Go to point', 'wall following', 'checking leave point', 'start', 'stop']
 state_ = 0
 count_state_time_ = 0 # seconds the robot is in a state
 count_loop_ = 0
 # 0 - go to point
 # 1 - wall following
 # 2 - checking leave point
-# 3 -start position
+# 3 - start position
+# 4 - stop
 
 # callbacks
 #robot movement callbacks
@@ -74,8 +74,14 @@ def change_state(state):
     global state_, state_desc_
     global srv_client_wall_follower_, srv_client_go_to_point_
     global count_state_time_
-    count_state_time_ = 0
+    global count_loop_
+
+    if not((state_ == 1 and state == 2) or (state_ == 2 and state == 1)):
+        count_state_time_ = 0
+    count_loop_ = 0
+    
     state_ = state
+
     #informing user that the state has changed
     log = "state changed: %s" % state_desc_[state]
     rospy.loginfo(log)
@@ -92,19 +98,9 @@ def change_state(state):
     if state_ == 3:
         resp = srv_client_go_to_point_(False)
         resp = srv_client_wall_follower_(False)
-    
-#fucntion to calculate distance to line connecting start and finish
-def distance_to_line(p0):
-    # p0 is the current position
-    # p1 and p2 points define the line
-    global st_position_, desired_position_
-    p1 = st_position_
-    p2 = desired_position_
-    up_eq = math.fabs((p2.y - p1.y) * p0.x - (p2.x - p1.x) * p0.y + (p2.x * p1.y) - (p2.y * p1.x))
-    lo_eq = math.sqrt(pow(p2.y - p1.y, 2) + pow(p2.x - p1.x, 2))
-    distance = up_eq / lo_eq
-    
-    return distance
+    if state_ == 4:
+        resp = srv_client_go_to_point_(False)
+        resp = srv_client_wall_follower_(False)
 
 #function to calculate distance betweeen two points
 def calc_dist_points(point1, point2):
@@ -119,12 +115,12 @@ def normalize_angle(angle):
 
 def main():
     # stating global parameters
-    global regions_, position_, desired_position_, state_, yaw_, yaw_error_allowed_, st_position_
+    global regions_, position_, desired_position_, state_, yaw_, st_position_
     global srv_client_go_to_point_, srv_client_wall_follower_
     global count_state_time_, count_loop_
     global st_point
     
-    rospy.init_node('distbug')
+    rospy.init_node('distbug_step')
     
     #publisher to change velocities
     pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
@@ -134,11 +130,11 @@ def main():
     sub_odom = rospy.Subscriber('/odom', Odometry, clbk_odom)
     
     #initialize servers for other scripts involved
-    rospy.wait_for_service('/go_to_point_switch')
+    rospy.wait_for_service('/go_to_point_switch_step')
     rospy.wait_for_service('/wall_follower_switch')
     rospy.wait_for_service('/gazebo/set_model_state')
     
-    srv_client_go_to_point_ = rospy.ServiceProxy('/go_to_point_switch', SetBool)
+    srv_client_go_to_point_ = rospy.ServiceProxy('/go_to_point_switch_step', SetBool)
     srv_client_wall_follower_ = rospy.ServiceProxy('/wall_follower_switch', SetBool)
     srv_client_set_model_state = rospy.ServiceProxy('/gazebo/set_model_state', SetModelState)
     
@@ -149,7 +145,15 @@ def main():
     change_state(3)
     
     st_position_ = position_
-       
+
+    #st
+    prev_step_pos = position_
+    step = 0.4
+    cur_step_dist = 0
+    count_steps = 0
+    stop = 0
+    #end
+
     #calculate the yaw to the destination point
     desired_yaw = math.atan2(desired_position_.y - position_.y, desired_position_.x - position_.x)
     err_yaw = desired_yaw - yaw_
@@ -173,7 +177,7 @@ def main():
         if regions_ == None:
             continue
         #calculate distance to line (s-f)
-        distance_position_to_line = distance_to_line(position_)
+        # distance_position_to_line = distance_to_line(position_)
         
         #check if robot arived at the dest point
         if math.sqrt((desired_position_.y - position_.y)**2 + (desired_position_.x - position_.x)**2) < 0.15 :
@@ -190,48 +194,103 @@ def main():
             #check if there is an obstacle forward
             if regions_['front'] > 0 and regions_['front'] < 0.25:
                 st_point = position_
+                # log = "st_point [%.4f; %.4f] | %.4f" % (st_point.x, st_point.y, calc_dist_points(st_point, desired_position_))
+                # rospy.loginfo(log)
                 change_state(1)
         
         #circumnavigate until either line is crossed again or closed loop was made then point cannot be reached
         elif state_ == 1:
-            if count_state_time_ > 20 and \
-               distance_position_to_line < 0.1 and calc_dist_points(position_, desired_position_)< calc_dist_points(st_point, desired_position_):
-                change_state(2)
-                
-            if count_state_time_ > 20 and calc_dist_points(st_point, position_) < 0.1:
+            # log = '%.4f' % (calc_dist_points(st_point, position_))
+            # rospy.loginfo(log)
+            
+            if count_state_time_ > 20 and calc_dist_points(st_point, position_) < 0.2:
                log = "point cannot be reached"
                rospy.loginfo(log)
                exit(0)
-             
+            
+            elif count_state_time_ > 20 and calc_dist_points(position_, desired_position_) < calc_dist_points(st_point, desired_position_):
+                change_state(2)
+
+
         #checking if this point can be identified as leave point, looking in the direction of the finish basically        
         elif state_ == 2:
                 
+            desired_yaw = math.atan2(desired_position_.y - position_.y, desired_position_.x - position_.x)
+            err_yaw = desired_yaw - yaw_
+            while not math.fabs(err_yaw) <= math.pi / 90:
+                twist_msg = Twist()
                 desired_yaw = math.atan2(desired_position_.y - position_.y, desired_position_.x - position_.x)
                 err_yaw = desired_yaw - yaw_
-                while not math.fabs(err_yaw) <= math.pi / 90:
-                 twist_msg = Twist()
-                 desired_yaw = math.atan2(desired_position_.y - position_.y, desired_position_.x - position_.x)
-                 err_yaw = desired_yaw - yaw_
-                 twist_msg.angular.z = 0.7 if err_yaw > 0 else -0.7
-                 pub.publish(twist_msg)
-                 twist_msg.angular.z = 0
-                 pub.publish(twist_msg)
+                twist_msg.angular.z = 0.7 if err_yaw > 0 else -0.7
+                pub.publish(twist_msg)
+                twist_msg.angular.z = 0
+                pub.publish(twist_msg)
+            
+            #when we turned to finish and theres an obstacle we continue to circumnavigate obstacle
+            if regions_['front'] > 0.1 and regions_['front'] < 0.25:
+                log = "not the leave point, continuing circumnavigating"
+                rospy.loginfo(log)
+                change_state(1)
+            else:
+                #if there is no obstacle right in front of the robot then we go towards the next obstacle
+                change_state(0)
+
+        # stopping after step 
+        elif state_ == 4:
+            twist_msg = Twist()
+            twist_msg.linear.x = 0
+            pub.publish(twist_msg)
+            if count_state_time_ > 1:
+                prev_step_pos = position_
+                cur_step_dist = 0
+                change_state(0)
                 
-                #when we turned to finish and theres an obstacle we continue to circumnavigate obstacle
-                if regions_['front'] > 0.1 and regions_['front'] < 0.25:
-                 log = "not the leave point, continuing circumnavigating"
-                 rospy.loginfo(log)
-                 change_state(1)
-                else:
-                 #if there is no obstacle right in front of the robot then we go towards the next obstacle
-                 change_state(0)
-                
-                
-                
+        # if calc_dist_points(position_, prev_step_pos) > step - 0.05:
+        #     twist_msg = Twist()
+        #     twist_msg.linear.x = 0
+        #     pub.publish(twist_msg)
+        
+        if state_ != 4:
+            cur_step_dist += calc_dist_points(position_, prev_step_pos)
+            prev_step_pos = position_
+            if cur_step_dist > step:
+                log = "%d | %.4f" % (count_steps,cur_step_dist)
+                rospy.loginfo(log)
+                change_state(4)
+        # cur_step_dist += calc_dist_points(position_, prev_step_pos)
+        # if cur_step_dist > step - 0.03:
+        #     count_steps += 1
+        #     log = "%d | %.4f" % (count_steps,cur_step_dist)
+        #     rospy.loginfo(log)
+        #     stop = 10
+        #     change_state(4)
+            # twist_msg = Twist()
+            # twist_msg.linear.x = 0
+            # pub.publish(twist_msg)
+
+        # if stop > 0:
+        #     twist_msg = Twist()
+        #     twist_msg.linear.x = 0
+        #     pub.publish(twist_msg)
+        #     if count_loop_ == 20:
+        #         stop -= 1
+
+            
         count_loop_ = count_loop_ + 1
         if count_loop_ == 20:
             count_state_time_ = count_state_time_ + 1
             count_loop_ = 0
+            # cur_step_dist += calc_dist_points(position_, prev_step_pos)
+            # prev_step_pos = position_
+
+        # if count_loop_ == 19:
+        #     log = '%d' % count_state_time_
+        #     rospy.loginfo(log)
+
+        # rospy.loginfo(count_loop_)
+        # if count_loop_ == 19:
+        #     log = '[%.4f; %.4f] %.4f | state %s' % (position_.x, position_.y, calc_dist_points(position_, desired_position_), state_)
+        #     rospy.loginfo(log)
             
         rate.sleep()
 
